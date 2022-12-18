c130651c0c693386ec4995891f8bae2075fed411
Tyler Landtroop <tlandtroop@ufl.edu>
2022-12-18 17:59:47 -0500

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Tyler Landtroop on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
